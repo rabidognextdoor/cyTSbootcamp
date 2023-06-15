9bfa7e524cc736ba954959fed3a01559a1692d27
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-15 09:08:06 -0700

    Updates Demo spec to use fixture data

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,6 +1,8 @@
 describe('Demo test', () => {
     it('Should visit a website', () => {
         cy.visitLoginpage();
-        cy.loginUI('username', 'password');
+        cy.fixture('loginData').then(({ username, password }) => {
+            cy.loginUI(username, password);
+        });
     });
 });
