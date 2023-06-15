445ff99adbc02f7260e029b7417084b9d1b9987c
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-15 11:26:27 -0700

    minor tweaks to demo.cy

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,6 +1,8 @@
 describe('Demo test', () => {
     it('Should visit a website', () => {
+        cy.setViewport(Cypress.env('viewportSize'));
         cy.visitLoginpage();
+        // cy.waitForSeconds(2);
         cy.fixture('loginData').then(({ username, password }) => {
             cy.loginUI(username, password);
         });
