9e843fa1ffb27099017e42ab7c594928e277fbf6
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-20 15:34:57 -0700

    Adds cucumber preprocessor and types

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from 'cypress';
 import mochawesome from 'cypress-mochawesome-reporter/plugin';
+import cucumber from 'cypress-cucumber-preprocessor';
 
 export default defineConfig({
     projectId: 'x5f4v6',
-    video: true,
+    video: false,
     chromeWebSecurity: false,
     waitForAnimations: true,
     env: {
         screenshotsFolder: './cypress/snapshots/actual',
         trashAssetsBeforeRuns: true,
         url: 'http://zero.webappsecurity.com/',
+        apiUrl: 'https://reqres.in/api/',
     },
     e2e: {
         reporter: 'cypress-mochawesome-reporter',
         defaultCommandTimeout: 5000,
+        pageLoadTimeout: 10000,
         setupNodeEvents(on, config) {
+            on('file:preprocessor', cucumber());
             mochawesome(on);
         },
     },
