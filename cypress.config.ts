a5e9bb7c3c9cf3ac1d0e83b4c504be60e7f53280
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-20 16:17:04 -0700

    Integrate cucumber preprocessor into cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'cypress';
-import mochawesome from 'cypress-mochawesome-reporter/plugin';
-import cucumber from 'cypress-cucumber-preprocessor';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+
+async function setupNodeEvents(
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+    await addCucumberPreprocessorPlugin(on, config);
+
+    on(
+        'file:preprocessor',
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        })
+    );
+    return config;
+}
 
 export default defineConfig({
     projectId: 'x5f4v6',
@@ -15,11 +31,9 @@ export default defineConfig({
     },
     e2e: {
         reporter: 'cypress-mochawesome-reporter',
+        specPattern: '**/*.feature',
         defaultCommandTimeout: 5000,
         pageLoadTimeout: 10000,
-        setupNodeEvents(on, config) {
-            on('file:preprocessor', cucumber());
-            mochawesome(on);
-        },
+        setupNodeEvents,
     },
 });
