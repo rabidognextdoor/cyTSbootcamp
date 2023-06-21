c36dc72ad4468b4776a1fb292b2a8b17b760f84f
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-21 11:30:27 -0700

    Adds new common steps into common.ts

--- cypress/support/step_definitions/common/common.ts
+++ cypress/support/step_definitions/common/common.ts
@@ -1,4 +1,4 @@
-import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('I want to wait for {int} milliseconds', (time: number) => {
     cy.wait(time);
@@ -6,4 +6,12 @@ Given('I want to wait for {int} milliseconds', (time: number) => {
 
 Given('I see {string} in the title', (title: string) => {
     cy.title().should('include', title);
+});
+
+Given('I see {string} in the url', (url: string) => {
+    cy.url().should('include', url);
+});
+
+When('I reload the browser', () => {
+    cy.reload();
 });
