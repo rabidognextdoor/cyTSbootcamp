8443875f474eaac20e83c4ddb23c2d71e74bcdb5
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-21 16:47:40 -0700

    Adds single element snapshot test to visual regression spec

--- cypress/e2e/visual-tests/visual.cy.ts
+++ cypress/e2e/visual-tests/visual.cy.ts
@@ -1,11 +1,21 @@
 const pages = ['http://example.com'];
 const sizes = { widths: [375, 768, 992, 1200] };
+const targetElem = 'h1';
 
 describe('Visual Regression', () => {
     pages.forEach(page => {
         it(`Should match ${page} in resolution defined in snapshot function`, () => {
             cy.visit(page);
             cy.snapResolution(`${page}`, sizes);
+        });
+    });
+});
+
+describe('Single element snapshot', () => {
+    it(`Should match the first ${targetElem} on ${pages[0]}`, () => {
+        cy.visit(pages[0]);
+        cy.snapElement(`The ${targetElem} of ${pages[0]}`, {
+            scope: targetElem,
         });
     });
 });
