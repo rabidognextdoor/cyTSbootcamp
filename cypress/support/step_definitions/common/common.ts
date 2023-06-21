eb2ed42595162f26fb0798d84c9ce52608d6c8b4
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-21 11:48:08 -0700

    Update gherkin keywords in common steps

--- cypress/support/step_definitions/common/common.ts
+++ cypress/support/step_definitions/common/common.ts
@@ -1,14 +1,14 @@
-import { Given, When } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('I want to wait for {int} milliseconds', (time: number) => {
     cy.wait(time);
 });
 
-Given('I see {string} in the title', (title: string) => {
+Then('I see {string} in the title', (title: string) => {
     cy.title().should('include', title);
 });
 
-Given('I see {string} in the url', (url: string) => {
+Then('I see {string} in the url', (url: string) => {
     cy.url().should('include', url);
 });
 
