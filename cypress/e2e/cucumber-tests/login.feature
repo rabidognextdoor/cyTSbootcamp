081ecafab99295fc2655b4021c0f690b58c25022
Kyle2.0 <rabidognextdoor@gmail.com>
2023-06-21 19:51:28 -0700

    UPdates login feature to use new auth related steps

--- cypress/e2e/cucumber-tests/login.feature
+++ cypress/e2e/cucumber-tests/login.feature
@@ -1,11 +1,19 @@
 Feature: Login to application
 
+  As an unauthorized user
+  I should not be able to navigate into a page protected by auth
   as an Invalid user
   I cannot log into the application
   As a Valid user
   I want to log into the application
 
-  Scenario: Invalid login
+  Scenario: As an Unauthorized user
+    Given I navigate to a page protected by authorization
+    Then I should be redirected with a 302 status code
+    Then I see "/login.html" in the url
+    And I see "Zero - Log in" in the title
+
+  Scenario: As an Invalid user
     Given I open login page
     And I want to wait for 2000 milliseconds
     And I see "Zero - Log in" in the title
@@ -15,9 +23,9 @@ Feature: Login to application
     And I submit login
     Then I should see error message
 
-  Scenario: Valid login
+  Scenario: As a Valid user
     Given I open login page
     When I fill username with "username"
     When I fill password with "password"
     When I submit login
-    Then I should see homepage
+    Then I should see account summary
--- cypress/e2e/cucumber-tests/login/loginSteps.ts
+++ cypress/e2e/cucumber-tests/login/loginSteps.ts
@@ -4,6 +4,9 @@ import LoginPage from './loginPage';
 Given('I open login page', () => {
     LoginPage.visit();
 });
+Given('I navigate to a page protected by authorization', () => {
+    cy.visit('http://zero.webappsecurity.com/bank/account-summary.html');
+});
 When('I fill username with {string}', (username: string) => {
     LoginPage.fillUsername(username);
 });
@@ -16,9 +19,20 @@ When('I submit login', () => {
 When('I check the keep me signed in box', () => {
     LoginPage.checkKeepSigned();
 });
-Then('I should see homepage', () => {
+Then('I should see account summary', () => {
     cy.get('#account_summary_tab').should('be.visible');
 });
 Then('I should see error message', () => {
     LoginPage.shouldShowError();
+});
+Then('I should be redirected with a 302 status code', () => {
+    cy.request({
+        url: 'http://zero.webappsecurity.com/bank/account-summary.html',
+        followRedirect: false,
+    }).then(resp => {
+        expect(resp.status).to.equal(302);
+        expect(resp.redirectedToUrl).to.equal(
+            `${Cypress.env('url')}login.html`
+        );
+    });
 });
